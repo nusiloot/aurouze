0571fddfe7d6d77996b7895a44d82aa492541c9b
Jb Lm <lemetayer.jb@gmail.com>
2016-01-27 16:34:44 +0100

    menage des bundles

--- project/app/AppKernel.php
+++ project/app/AppKernel.php
@@ -16,8 +16,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
-        	new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-        	new ADesigns\CalendarBundle\ADesignsCalendarBundle(),
             new AppBundle\AppBundle(),
             
         );
