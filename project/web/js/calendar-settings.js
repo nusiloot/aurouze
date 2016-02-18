a9cdc6364360554a32d7a1d4583a715ab3fa47fc
Mathurin <mathurin.petit@gmail.com>
2016-02-18 20:38:09 +0100

    retour depuis calendar dans les passage de un tech

--- project/web/js/calendar-settings.js
+++ project/web/js/calendar-settings.js
@@ -50,6 +50,7 @@ $(function () {
             );
         },
         eventReceive: function (event) {
+            $('#retour_technicien_btn').removeClass('hidden');
             $.post(
                     $('#calendrier').data('urlUpdate'), {
                 id: null,
@@ -65,15 +66,15 @@ $(function () {
         },
         eventResize: function (event) {
             $.post(
-                    $('#calendrier').data('urlUpdate'), {
+               $('#calendrier').data('urlUpdate'), {
                 id: event.id,
                 start: event.start.format(),
                 end: event.end.format()
             });
         },
-        eventDrop: function (event) {
+        eventDrop: function (event) {           
             $.post(
-                    $('#calendrier').data('urlUpdate'), {
+               $('#calendrier').data('urlUpdate'), {
                 id: event.id,
                 start: event.start.format(),
                 end: event.end.format()
