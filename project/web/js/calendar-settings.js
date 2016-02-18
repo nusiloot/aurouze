6211ab12964bad85d048c52ab0d1dd3f7b1ea44a
Mathurin <mathurin.petit@gmail.com>
2016-02-18 16:55:04 +0100

    cantonnage du calendar de 6h du mat à 18h

--- project/web/js/calendar-settings.js
+++ project/web/js/calendar-settings.js
@@ -1,80 +1,83 @@
 $(function () {
-	/**
-	 * Dropping Elements
-	 */
-	$('#fc-events .event').each(function() {
-		$(this).data('event', {
-			title: $.trim($(this).data('title')),
-			stick: true
-		});
-		$(this).draggable({
-			zIndex: 999,
-			revert: true,
-			revertDuration: 0 
-		});
+    /**
+     * Dropping Elements
+     */
+    $('#fc-events .event').each(function () {
+        $(this).data('event', {
+            title: $.trim($(this).data('title')),
+            stick: true
+        });
+        $(this).draggable({
+            zIndex: 999,
+            revert: true,
+            revertDuration: 0
+        });
 
-	});
-	/**
-	 * FullCalendar Settings
-	 */
+    });
+    /**
+     * FullCalendar Settings
+     */
     $('#calendrier').fullCalendar({
+        minTime: '06:00:00',
+        maxTime: '18:00:00',
+        height: 600,
         header: {
             left: 'prev, title, next',
-            right: 'month, agendaWeek, agendaDay'
+            right: 'month, agendaWeek, agendaDay',
         },
         lang: 'fr',
-		timeFormat: 'H:mm',
-		allDaySlot: false,
-		editable: true,
-		droppable: true,
-		hiddenDays: [0],
-		defaultView: "agendaWeek",
+        timeFormat: 'H:mm',
+        allDaySlot: false,
+        editable: true,
+        droppable: true,
+        hiddenDays: [0],
+        defaultView: "agendaWeek",
         eventSources: [
             {
                 url: $('#calendrier').data('urlPopulate'),
                 type: 'POST'
             }
         ],
-        eventClick: function(event) {
-        		$.post(
-        			$('#calendrier').data('urlRead'), { 
-        			id: event.id
-        		}, function(response) {
-        			$('#modal-title').text(event.title);
-        			$('#modal-body').html(response);
-        			$('#modal-calendrier-infos').modal();
-        		}
-        	);
+        eventClick: function (event) {
+            $.post(
+                    $('#calendrier').data('urlRead'), {
+                id: event.id
+            }, function (response) {
+                $('#modal-title').text(event.title);
+                $('#modal-body').html(response);
+                $('#modal-calendrier-infos').modal();
+            }
+            );
         },
-        eventReceive: function(event) {
-        	$.post(
-        			$('#calendrier').data('urlUpdate'), { 
-        			id: null, 
-        			start: event.start.format(),
-        			end: event.end.format()
-        		}, function(data) {
-        			event.id = data.id;
-        		}
-        	);
+        eventReceive: function (event) {
+            $.post(
+                    $('#calendrier').data('urlUpdate'), {
+                id: null,
+                start: event.start.format(),
+                end: event.end.format()
+            }, function (data) {
+                event.id = data.id;
+            }
+            );
         },
-        drop: function() {
-				$(this).remove();
-		},
-        eventResize: function(event) {
-        	$.post(
-        			$('#calendrier').data('urlUpdate'), { 
-        			id: event.id, 
-        			start: event.start.format(),
-        			end: event.end.format()
-        		});
+        drop: function () {
+            $(this).remove();
         },
-        eventDrop: function(event) {  
-        	$.post(
-        			$('#calendrier').data('urlUpdate'), { 
-        			id: event.id, 
-        			start: event.start.format(),
-        			end: event.end.format()
-        		});
+        eventResize: function (event) {
+            $.post(
+                    $('#calendrier').data('urlUpdate'), {
+                id: event.id,
+                start: event.start.format(),
+                end: event.end.format()
+            });
+        },
+        eventDrop: function (event) {
+            $.post(
+                    $('#calendrier').data('urlUpdate'), {
+                id: event.id,
+                start: event.start.format(),
+                end: event.end.format()
+            });
         },
     });
 });
