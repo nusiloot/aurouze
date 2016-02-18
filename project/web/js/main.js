bbbea39620a963dc2923e43fcb4a40b614db8044
Vincent LAURENT <vince.laurent@gmail.com>
2016-02-18 19:27:16 +0100

    Hamza style dans les passages

--- project/web/js/main.js
+++ project/web/js/main.js
@@ -7,6 +7,7 @@
         $.initSelect2();
         $.initSelect2Ajax();
         $.initTooltips();
+        $.initQueryHash();
     });
     
     $.initTooltips = function () {
@@ -109,4 +110,90 @@
             $(this).parents('form').submit();
         }
     });
+
+    $('.hamzastyle').each(function () {
+        var select2 = $(this);
+        var words = [];
+        $('.hamzastyle-item').each(function () {
+            words = words.concat(JSON.parse($(this).attr('data-words')));
+        });
+        var unique = function(a) {var n = {},r=[];for(var i = 0; i < a.length; i++) {if (!n[a[i]]) {n[a[i]] = true; r.push(a[i]); }}return r;};
+        var words = unique(words.sort());
+
+        var data = [];
+        for (key in words) {
+            if (words[key] + "") {
+                data.push({id: (words[key] + ""), text: (words[key] + "")});
+            }
+        }
+
+        select2.select2({
+            multiple: true,
+            data: data
+        })
+    });
+
+    $(document).find('.hamzastyle').on("change", function (e) {
+        var select2Data = $(this).select2("data");
+        var selectedWords = [];
+        for (key in select2Data) {
+            selectedWords.push(select2Data[key].text);
+        }
+
+        if (!selectedWords.length) {
+            document.location.hash = "";
+        } else {
+            document.location.hash = encodeURI("#filtre=" + JSON.stringify(selectedWords));
+        }
+    });
+
+    $.initQueryHash = function () {
+        $(window).on('hashchange', function () {
+            if ($(document).find('.hamzastyle').length) {
+                var params = jQuery.parseParams(location.hash.replace("#", ""));
+                var filtres = [];
+                if (params.filtre && params.filtre.match(/\[/)) {
+                    filtres = JSON.parse(params.filtre);
+                } else if (params.filtre) {
+                    filtres.push(params.filtre);
+                }
+
+                var select2Data = [];
+                for (key in filtres) {
+                    select2Data.push({id: filtres[key], text: filtres[key]});
+                }
+
+                $(document).find('.hamzastyle').select2("data", select2Data);
+
+                $(document).find('.hamzastyle-item').each(function () {
+                    var words = $(this).attr('data-words');
+                    var find = true;
+                    for (key in filtres) {
+                        var word = filtres[key];
+                        if (words.indexOf(word) === -1) {
+                            find = false;
+                        }
+                    }
+                    if (find) {
+                        $(this).show();
+                    } else {
+                        $(this).hide();
+                    }
+                });
+            }
+            if ($(document).find('.nav.nav-tabs').length) {
+                var params = jQuery.parseParams(location.hash.replace("#", ""));
+                if (params.tab) {
+                    $('.nav.nav-tabs a[aria-controls="' + params.tab + '"]').tab('show');
+                }
+            }
+        });
+
+        if (location.hash) {
+            $(window).trigger('hashchange');
+        }
+    }
+    
 })(jQuery);
+
+(function($) { var re = /([^&=]+)=?([^&]*)/g; var decodeRE = /\+/g; var decode = function (str) {return decodeURIComponent( str.replace(decodeRE, " ") );}; $.parseParams = function(query) { var params = {}, e; while ( e = re.exec(query) ) { var k = decode( e[1] ), v = decode( e[2] ); if (k.substring(k.length - 2) === '[]') { k = k.substring(0, k.length - 2); (params[k] || (params[k] = [])).push(v); } else params[k] = v; } return params; }; })(jQuery);
