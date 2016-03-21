f17b6a637bdcf08041a477f437a5a25b1a8b7a9e
Mathurin <mathurin.petit@gmail.com>
2016-03-21 10:31:01 +0100

    linking contrat/passage, routing contrat par ID la création du passage rempli les infos techniciens

--- project/src/AppBundle/Controller/ContratController.php
+++ project/src/AppBundle/Controller/ContratController.php
@@ -8,82 +8,94 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Document\Etablissement;
 use AppBundle\Document\Contrat;
+use AppBundle\Document\UserInfos;
 use AppBundle\Type\ContratType;
 
 class ContratController extends Controller {
 
     /**
-     * @Route("/contrat/{identifiantEtablissement}/creation", name="contrat_creation")
+     * @Route("/contrat/{id}/creation", name="contrat_creation")
      */
-    public function creationAction(Request $request, $identifiantEtablissement) {
+    public function creationAction(Request $request, $id) {
         $dm = $this->get('doctrine_mongodb')->getManager();
-        $etablissement = $dm->getRepository('AppBundle:Etablissement')->findOneByIdentifiant($identifiantEtablissement);
-        $contrat = $this->get('contrat.manager')->create($etablissement);        
+        $etablissement = $dm->getRepository('AppBundle:Etablissement')->findOneById($id);
+        $contrat = $this->get('contrat.manager')->create($etablissement);
         $dm->persist($contrat);
         $dm->flush();
-        return $this->redirectToRoute('contrat_modification', array('identifiantContrat' => $contrat->getIdentifiant()));
+        return $this->redirectToRoute('contrat_modification', array('id' => $contrat->getId()));
     }
 
     /**
-     * @Route("/contrat/{identifiantContrat}/modification", name="contrat_modification")
+     * @Route("/contrat/{id}/modification", name="contrat_modification")
      */
-    public function modificationAction(Request $request, $identifiantContrat) {
+    public function modificationAction(Request $request, $id) {
 
         $dm = $this->get('doctrine_mongodb')->getManager();
-        $contrat = $dm->getRepository('AppBundle:Contrat')->findOneByIdentifiant($identifiantContrat);
+        $contrat = $dm->getRepository('AppBundle:Contrat')->findOneById($id);
+      
         $form = $this->createForm(new ContratType($this->container, $dm), $contrat, array(
-            'action' => $this->generateUrl('contrat_modification', array('identifiantContrat' => $identifiantContrat)),
+            'action' => $this->generateUrl('contrat_modification', array('id' => $id)),
             'method' => 'POST',
         ));
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $contrat = $form->getData();            
+            $contrat = $form->getData();
+           
             $nextPassage = $contrat->getNextPassage();
             if ($nextPassage) {
+                $userInfos = new UserInfos();
+                $user = $dm->getRepository('AppBundle:User')->findOneById($contrat->getTechnicien()->getId());
+                if ($user) {
+                    $userInfos->copyFromUser($user);
+                } else {
+                    $userInfos->setCouleur("#ffffff");
+                    $userInfos->setIdentite($data[self::CSV_TECHNICIEN]);
+                }
+                $nextPassage->setTechnicienInfos($userInfos);
                 $contrat->addPassage($nextPassage);
                 $dm->persist($nextPassage);
             }
             $dm->persist($contrat);
             $dm->flush();
-            return $this->redirectToRoute('contrat_validation', array('identifiantContrat' => $contrat->getIdentifiant()));
+            return $this->redirectToRoute('contrat_validation', array('id' => $contrat->getId()));
         }
         return $this->render('contrat/modification.html.twig', array('contrat' => $contrat, 'form' => $form->createView()));
     }
 
     /**
-     * @Route("/contrat/{identifiantContrat}/validation", name="contrat_validation")
+     * @Route("/contrat/{id}/validation", name="contrat_validation")
      */
-    public function validationAction(Request $request, $identifiantContrat) {
+    public function validationAction(Request $request, $id) {
         $dm = $this->get('doctrine_mongodb')->getManager();
-        $contrat = $dm->getRepository('AppBundle:Contrat')->findOneByIdentifiant($identifiantContrat);
+        $contrat = $dm->getRepository('AppBundle:Contrat')->findOneById($id);
         if ($request->get('valide')) {
             $contrat->setStatut(Contrat::STATUT_VALIDE);
             $dm->persist($contrat);
             $dm->flush();
-            return $this->redirectToRoute('contrat_visualisation', array('identifiantContrat' => $contrat->getIdentifiant()));
+            return $this->redirectToRoute('contrat_visualisation', array('id' => $contrat->getId()));
         }
         return $this->render('contrat/validation.html.twig', array('contrat' => $contrat));
     }
 
     /**
-     * @Route("/contrat/{identifiantContrat}/visualisation", name="contrat_visualisation")
+     * @Route("/contrat/{id}/visualisation", name="contrat_visualisation")
      */
-    public function visualisationAction(Request $request, $identifiantContrat) {
+    public function visualisationAction(Request $request, $id) {
         $dm = $this->get('doctrine_mongodb')->getManager();
-        $contrat = $dm->getRepository('AppBundle:Contrat')->findOneByIdentifiant($identifiantContrat);
+        $contrat = $dm->getRepository('AppBundle:Contrat')->findOneById($id);
         return $this->render('contrat/visualisation.html.twig', array('contrat' => $contrat));
     }
 
     /**
-     * @Route("/contrat/{identifiantContrat}/suppression", name="contrat_suppression")
+     * @Route("/contrat/{id}/suppression", name="contrat_suppression")
      */
-    public function suppressionAction(Request $request, $identifiantContrat) {
+    public function suppressionAction(Request $request, $id) {
         $dm = $this->get('doctrine_mongodb')->getManager();
-        $contrat = $dm->getRepository('AppBundle:Contrat')->findOneByIdentifiant($identifiantContrat);
-        $etablissementIdentifiant = $contrat->getEtablissement()->getIdentifiant();
+        $contrat = $dm->getRepository('AppBundle:Contrat')->findOneById($id);
+        $etablissementId = $contrat->getEtablissement()->getId();
         $dm->remove($contrat);
         $dm->flush();
-        return $this->redirectToRoute('passage_etablissement', array('identifiantEtablissement' => $etablissementIdentifiant));
+        return $this->redirectToRoute('passage_etablissement', array('id' => $etablissementId));
     }
 
 }
