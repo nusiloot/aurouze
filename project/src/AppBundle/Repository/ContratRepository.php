c3d9469317423004cd8d6908d2f299c682de0c92
Vincent LAURENT <vince.laurent@gmail.com>
2016-04-05 18:52:23 +0200

    Utilisation de la strategy de méthode du contrat pour l'id

--- project/src/AppBundle/Repository/ContratRepository.php
+++ project/src/AppBundle/Repository/ContratRepository.php
@@ -12,30 +12,11 @@ class ContratRepository extends DocumentRepository {
                         array('societe.id' => $societe->getId()),
                         array('dateFin' => 'DESC'));
     }
-    
+
     public function findByEtablissement($etablissement) {
         return $this->findBy(
                         array('societe.id' => $etablissement->getSociete()->getId()),
                         array('dateFin' => 'DESC'));
-    }
-
-    public function findBySocieteAndDateCreation($societe, $dateCreation) {
-        $contratsBySociete = $this->findBy(array("societe.id" => $societe->getId()));
-        $results = array();
-        foreach ($contratsBySociete as $contrat) {
-            if($contrat->getDateCreation()->format('Ymd') == $dateCreation->format('Ymd'))
-            $results[] = $contrat;
-        }
-        return $results;
-    }
-
-    public function findNextNumero($societe, $dateCreation) {
-        $contrats = $this->findBySocieteAndDateCreation($societe, $dateCreation);
-        $identifiants = array();
-        foreach ($contrats as $contrat) {
-            $identifiants[$contrat->getIdentifiant()] = $contrat->getIdentifiant();
-        }
-        return (count($identifiants) > 0) ? (substr(max($identifiants), -3) + 1) : 1;
     }
 
     public function findContratMouvements($etablissement, $isFacturable, $isFacture) {
