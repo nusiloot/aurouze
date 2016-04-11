4fa330d4c80719cec075a080ba49fd3ffe73eb7f
Mathurin <mathurin.petit@gmail.com>
2016-04-11 12:11:52 +0200

    merdge

--- project/src/AppBundle/Document/Contrat.php
+++ project/src/AppBundle/Document/Contrat.php
@@ -146,7 +146,7 @@ class Contrat {
     protected $identifiantReprise;
 
     public function __construct() {
-        $this->prestations = new ArrayCollection();
+        //$this->prestations = new ArrayCollection();
         $this->mouvements = new ArrayCollection();
     }
 
