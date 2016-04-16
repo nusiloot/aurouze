da3a3c13e315cef1162e1fcfe205416ba4dd61e2
Vincent LAURENT <vince.laurent@gmail.com>
2016-04-16 15:21:19 +0200

    Amélioration des interfaces

--- project/src/AppBundle/Manager/FactureManager.php
+++ project/src/AppBundle/Manager/FactureManager.php
@@ -33,7 +33,7 @@ class FactureManager {
 
     public function findBySociete(Societe $societe) {
 
-        return $this->getRepository()->findBy(array('societe.id' => $societe->getId()));
+        return $this->getRepository()->findBy(array('societe.id' => $societe->getId()), array('dateEmission' => 'desc'));
     }
 
     public function create(Societe $societe, $mouvements, $dateFacturation) {
