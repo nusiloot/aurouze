2d62564026502ade8d5d3611aced63831e5f48d5
Vincent LAURENT <vince.laurent@gmail.com>
2016-05-09 08:43:27 +0200

    Fix bug

--- project/src/AppBundle/Repository/PassageRepository.php
+++ project/src/AppBundle/Repository/PassageRepository.php
@@ -22,7 +22,7 @@ class PassageRepository extends DocumentRepository {
                 ->field('dateDebut')->gte($mongoStartDate)
                 ->field('dateDebut')->lte($mongoEndDate)
                 ->field('dateFin')->gte($mongoStartDate)
-                ->field('techniciens.$id')->equals($technicien->getId())
+                ->field('techniciens')->equals($technicien->getId())
                 ->sort('dateDebut', 'asc')
                 ->getQuery();
         return $query->execute();
