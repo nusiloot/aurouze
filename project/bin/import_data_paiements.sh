b9f3847d244e7c1969bfe7a8581e6fa2a3772fc2
Mathurin <mathurin.petit@gmail.com>
2016-05-21 14:12:10 +0200

    import des paiements

--- project/bin/import_data_paiements.sh
+++ project/bin/import_data_paiements.sh
@@ -32,7 +32,11 @@ cat $DATA_DIR/tblReglement.csv | tr -d "\r" | sort -t ';' -k 3,3 | sed -r 's/([a
 
 cat $DATA_DIR/tblPieceBanque.csv | tr -d "\r" | sort -t ';' -k 1,1 | sed -r 's/([a-zA-Z]+)[ ]+([0-9]+)[ ]+([0-9]+)[ ]+([0-9:]+):[0-9]{3}([A-Z]{2})/\1 \2 \3 \4 \5/g' > $DATA_DIR/piecesBanques.clean.csv
 
-join -a 1 -t ';' -1 3 -2 1 -o auto $DATA_DIR/reglements.clean.csv $DATA_DIR/piecesBanques.clean.csv > $DATA_DIR/paiements.csv
+join -a 1 -t ';' -1 3 -2 1 -o auto $DATA_DIR/reglements.clean.csv $DATA_DIR/piecesBanques.clean.csv | sort -t ';' -k 13,13  > $DATA_DIR/paiementsClean.csv
+
+cat $DATA_DIR/tblRemiseCheque.csv | tr -d "\r" | sort -t ';' -k 1,1 | sed -r 's/([a-zA-Z]+)[ ]+([0-9]+)[ ]+([0-9]+)[ ]+([0-9:]+):[0-9]{3}([A-Z]{2})/\1 \2 \3 \4 \5/g' > $DATA_DIR/cheques.clean.csv
+
+join -a 1 -t ';' -1 13 -2 1 $DATA_DIR/paiementsClean.csv $DATA_DIR/cheques.clean.csv > $DATA_DIR/paiements.csv
 
 echo -e "\n\nImport des types de prestations\n"
 
