57b88d937f4023435bce40eb68cdda0a006af806
Vincent LAURENT <vince.laurent@gmail.com>
2016-07-06 20:08:46 +0200

    Facture sur 2 pages

--- project/src/AppBundle/Controller/FactureController.php
+++ project/src/AppBundle/Controller/FactureController.php
@@ -227,8 +227,82 @@ class FactureController extends Controller {
     public function pdfAction(Request $request, Facture $facture) {
         $fm = $this->get('facture.manager');
 
+        $pages = array();
+
+        $nbLigneMaxPourPageVierge = 50;
+        $nbLigneMaxPourDernierePage = 30;
+        $nbPage = 1;
+        $nbMaxCharByLigne = 60;
+        $nbCurrentLigne = 0;
+        $nbCurrentPage = 1;
+        $nbLigneParLigneFacture = array();
+        $nbLigneParPage = array(1 => $nbLigneMaxPourDernierePage);
+
+        foreach($facture->getLignes() as $key => $ligne) {
+            $nbCurrentLigne += 2;
+            if($ligne->getReferenceClient()) {
+                $nbCurrentLigne += 1;
+            }
+
+            if($ligne->isOrigineContrat()) {
+                $nbCurrentLigne += 4;
+                $nbCurrentLigne += count($ligne->getOrigineDocument()->getPrestations());
+                $nbCurrentLigne += count($ligne->getOrigineDocument()->getContratPassages());
+            }
+
+            $nbLigneParLigneFacture[$key] = $nbCurrentLigne;
+
+            if($nbCurrentPage == $nbPage && $nbCurrentLigne > $nbLigneMaxPourDernierePage) {
+                $nbLigneParPage[$nbCurrentPage] = $nbLigneMaxPourDernierePage;
+                $nbPage += 1;
+                $nbLigneParPage[$nbPage] = $nbLigneMaxPourDernierePage;
+            }
+
+            if($nbCurrentPage < $nbPage && $nbCurrentLigne > $nbLigneMaxPourPageVierge) {
+                $nbLigneParPage[$nbCurrentPage] = $nbLigneMaxPourPageVierge;
+                $nbCurrentPage += 1;
+                $nbCurrentLigne = 0;
+            }
+
+        }
+
+        $nbCurrentPage = 1;
+        $nbCurrentLigne = 0;
+        foreach($facture->getLignes() as $key => $ligneFacture) {
+
+            $ligne = $this->buildLignePDFFacture($ligneFacture);
+
+            // La ligne ne tient pas sur une page complète
+            if(($nbLigneParLigneFacture[$key]) > $nbLigneParPage[$nbCurrentPage]) {
+                $nbLignes2Keep = (int)(0.7 * $nbLigneParPage[$nbCurrentPage]);
+                $lignesSplitted = $this->splitLigne($ligne, $nbLignes2Keep);
+                $pages[$nbCurrentPage][] = $lignesSplitted[0];
+                $pages[$nbCurrentPage+1][] = $lignesSplitted[1];
+                $nbCurrentLigne = 0;
+                $nbCurrentPage += 1;
+                continue;
+            }
+
+            // La ligne tient sur la page
+            if(($nbCurrentLigne + $nbLigneParLigneFacture[$key]) < $nbLigneParPage[$nbCurrentPage]) {
+
+                $pages[$nbCurrentPage][] = $ligne;
+                continue;
+            }
+
+            // La ligne ne tient plus sur la page
+            if(($nbCurrentLigne + $nbLigneParLigneFacture[$key]) > $nbLigneParPage[$nbCurrentPage]) {
+
+                $nbCurrentLigne = 0;
+                $nbCurrentPage += 1;
+                $pages[$nbCurrentPage][] = $ligne;
+                continue;
+            }
+        }
+
         $html = $this->renderView('facture/pdf.html.twig', array(
             'facture' => $facture,
+            'pages' => $pages,
             'parameters' => $fm->getParameters(),
         ));
 
@@ -255,6 +329,67 @@ class FactureController extends Controller {
             'Content-Disposition' => 'attachment; filename="' . $filename . '"',
                 )
         );
+    }
+
+    public function splitLigne($ligne, $nbLignes2Keep) {
+        $ligneSplitted = array();
+
+        $ligneSplitted["libelle"] = $ligne['libelle']." (Suite)";
+        foreach($ligne["details"] as $key => $details) {
+            if(!preg_match("/^Lieu/", $key)) {
+                continue;
+            }
+            $nb = 0;
+            $keySplitted = $key." (Suite)";
+            $ligneSplitted["details"] = array();
+            $ligneSplitted["details"][$keySplitted] = array();
+            foreach($details as $keyLieu => $lieu) {
+                $nb += 1;
+                if($nb <= $nbLignes2Keep) {
+                    continue;
+                }
+                $ligneSplitted["details"][$keySplitted][] = $lieu;
+                unset($ligne["details"][$key][$keyLieu]);
+            }
+            $ligne["details"][$key][] = "(Suite de la liste sur la page suivante)";
+        }
+
+        return array($ligne, $ligneSplitted);
+    }
+
+    public function buildLignePDFFacture($ligneFacture) {
+        $ligne = array();
+        $ligne['libelle'] = $ligneFacture->getLibelle();
+        $ligne['quantite'] = $ligneFacture->getQuantite();
+        $ligne['prixUnitaire'] = $ligneFacture->getPrixUnitaire();
+        $ligne['montantHT'] = $ligneFacture->getMontantHT();
+        $ligne['referenceClient'] = $ligneFacture->getReferenceClient();
+        if($ligneFacture->isOrigineContrat()) {
+            $ligne['details'] = array();
+
+            $keyPrestation = "Prestation";
+            if (count($ligneFacture->getOrigineDocument()->getPrestations()) > 1) { $keyPrestation .= "s"; }
+            foreach($ligneFacture->getOrigineDocument()->getPrestations() as $prestation) {
+                $ligne['details'][$keyPrestation][] = $prestation->getNom();
+            }
+
+            $keyPassage = "Lieu";
+            if(count($ligneFacture->getOrigineDocument()->getContratPassages()) > 1) { $keyPassage .= "x"; }
+            $keyPassage .= " d'application";
+            if(count($ligneFacture->getOrigineDocument()->getContratPassages()) > 1) { $keyPassage .= "s"; }
+            foreach($ligneFacture->getOrigineDocument()->getContratPassages() as $passage) {
+               $lignePassage = $passage->getEtablissement()->getNom(false).", ";
+               if($passage->getEtablissement()->getAdresse()->getAdresse()){ $lignePassage .= $passage->getEtablissement()->getAdresse()->getAdresse().", "; }
+               $lignePassage .= $passage->getEtablissement()->getAdresse()->getCodePostal()." ".$passage->getEtablissement()->getAdresse()->getCommune();
+               $ligne['details'][$keyPassage][] = $lignePassage;
+            }
+
+            if($ligneFacture->getDescription()) {
+                $ligne["details"]["description"] = $ligneFacture->getDescription();
+            }
+        }
+
+        return $ligne;
     }
 
     public function getPdfGenerationOptions() {
