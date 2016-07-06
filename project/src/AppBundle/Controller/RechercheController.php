16c4299d885315cdc786ed781af3b88b7a410ffe
Vincent LAURENT <vince.laurent@gmail.com>
2016-07-06 10:25:57 +0200

    Correction d'un bug dans la requête

--- project/src/AppBundle/Controller/RechercheController.php
+++ project/src/AppBundle/Controller/RechercheController.php
@@ -12,37 +12,37 @@ class RechercheController extends Controller {
 	/**
 	 * @Route("/recherche", name="recherche")
 	 */
-	public function indexAction(Request $request) 
+	public function indexAction(Request $request)
 	{
 		$dm = $this->get('doctrine_mongodb')->getManager();
         $query = $request->get('q');
-        
+
         $result = array_merge($dm->getRepository('AppBundle:Societe')->findByQuery($query), $dm->getRepository('AppBundle:Etablissement')->findByQuery($query));
         $result = array_merge($result, $dm->getRepository('AppBundle:Compte')->findByQuery($query));
-        
+
         usort($result, array("AppBundle\Controller\RechercheController", "cmpContacts"));
-        
+
 		return $this->render('recherche/index.html.twig', array('query' => $query, 'result' => $result));
 	}
 
 	/**
 	 * @Route("/recherche/societe", name="recherche_societe")
 	 */
-	public function societeAction(Request $request) 
+	public function societeAction(Request $request)
 	{
 		$dm = $this->get('doctrine_mongodb')->getManager();
         $query = $request->get('q');
         $inactif = $request->get('inactif', false);
         $inactif = ($inactif)? true : false;
-        
+
         $result = array_merge($dm->getRepository('AppBundle:Societe')->findByQuery($query, $inactif), $dm->getRepository('AppBundle:Etablissement')->findByQuery($query, $inactif));
         $result = array_merge($result, $dm->getRepository('AppBundle:Compte')->findByQuery($query, $inactif));
         usort($result, array("AppBundle\Controller\RechercheController", "cmpContacts"));
-        
+
 
         $result = $this->contructSearchResultSociete($result);
-        
-        $response = new Response(); 
+
+        $response = new Response();
         $response->headers->set('Content-Type', 'application/json');
         $response->setContent(json_encode($result));
         return $response;
@@ -51,18 +51,18 @@ class RechercheController extends Controller {
 	/**
 	 * @Route("/recherche/contrat", name="recherche_contrat")
 	 */
-	public function contratAction(Request $request) 
+	public function contratAction(Request $request)
 	{
 		$dm = $this->get('doctrine_mongodb')->getManager();
         $query = $request->get('q');
-        
+
         $result = $dm->getRepository('AppBundle:Contrat')->findByQuery($query);
         usort($result, array("AppBundle\Controller\RechercheController", "cmpContacts"));
-        
+
 
         $result = $this->contructSearchResultContrat($result);
-        
-        $response = new Response(); 
+
+        $response = new Response();
         $response->headers->set('Content-Type', 'application/json');
         $response->setContent(json_encode($result));
         return $response;
@@ -71,29 +71,29 @@ class RechercheController extends Controller {
 	/**
 	 * @Route("/recherche/facture", name="recherche_facture")
 	 */
-	public function factureAction(Request $request) 
+	public function factureAction(Request $request)
 	{
 		$dm = $this->get('doctrine_mongodb')->getManager();
         $query = $request->get('q');
-        
+
         $result = $dm->getRepository('AppBundle:Facture')->findByQuery($query);
         usort($result, array("AppBundle\Controller\RechercheController", "cmpContacts"));
-        
+
 
         $result = $this->contructSearchResultFacture($result);
-        
-        $response = new Response(); 
+
+        $response = new Response();
         $response->headers->set('Content-Type', 'application/json');
         $response->setContent(json_encode($result));
         return $response;
 	}
-	
-	public static function cmpContacts($a, $b) 
+
+	public static function cmpContacts($a, $b)
 	{
 		return ($a['score'] > $b['score']) ? -1 : +1;
 	}
 
-    public function contructSearchResultSociete($items) 
+    public function contructSearchResultSociete($items)
     {
 		$result = array();
         foreach ($items as $item) {
@@ -110,7 +110,7 @@ class RechercheController extends Controller {
         return $result;
     }
 
-    public function contructSearchResultContrat($items) 
+    public function contructSearchResultContrat($items)
     {
 		$result = array();
         foreach ($items as $item) {
@@ -122,7 +122,13 @@ class RechercheController extends Controller {
             $newResult->color = $object->getStatutCouleur();
             $newResult->identifiant = $object->getNumeroArchive();
             $newResult->type = $object->getTypeContratLibelle();
-            $newResult->periode = $object->getDateDebut()->format('M Y').'&nbsp;-&nbsp;'.$object->getDateFin()->format('M Y');
+			$newResult->periode = "";
+			if($object->getDateDebut()) {
+				$newResult->periode = $object->getDateDebut()->format('M Y');
+			}
+			if($object->getDateFin()) {
+				$newResult->periode .= '&nbsp;-&nbsp;'.$object->getDateFin()->format('M Y');
+			}
             $newResult->prix = $object->getPrixHt();
             $newResult->garantie = ($object->getDureeGarantie())? 'Garantie&nbsp;'.$object->getDureeGarantie().'&nbsp;mois' : 'Aucune ganrantie';
             $result[] = $newResult;
@@ -130,7 +136,7 @@ class RechercheController extends Controller {
         return $result;
     }
 
-    public function contructSearchResultFacture($items) 
+    public function contructSearchResultFacture($items)
     {
 		$result = array();
         foreach ($items as $item) {
@@ -142,6 +148,6 @@ class RechercheController extends Controller {
         }
         return $result;
     }
-	
-	
+
+
 }
