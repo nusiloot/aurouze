8948d82b7eeb85f47e9d98b8b3b6699b3e838804
Mathurin Petit <mathurin.petit@gmail.com>
2016-12-08 12:11:44 +0100

    adapation inté tournée

--- project/src/AppBundle/Type/PassageMobileType.php
+++ project/src/AppBundle/Type/PassageMobileType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Doctrine\ODM\MongoDB\DocumentManager;
 
 class PassageMobileType extends AbstractType
