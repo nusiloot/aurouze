8f76d824e3d2b1ce83e0e67074f4477019c34fde
Jb Lm <lemetayer.jb@gmail.com>
2017-01-19 17:21:18 +0100

    Bug adresse

---
+++ project/src/AppBundle/Command/UpdateEtablissementsAdressesCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ImportEtablissement
+ *
+ * @author mathurin
+ */
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
+use AppBundle\Manager\ContratManager;
+
+class UpdateEtablissementsAdressesCommand extends ContainerAwareCommand {
+
+    protected $dm;
+
+    protected function configure() {
+        $this
+                ->setName('update:etablissements-adresses')
+                ->setDescription('Mise à jour des coordonnees adresses établissements');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+
+        $this->dm = $this->getContainer()->get('doctrine_mongodb.odm.default_document_manager');
+
+        $i=0;
+        $j = 0;
+        foreach ($this->dm->getRepository('AppBundle:Etablissement')->findAll() as $etablissement) {
+            echo $etablissement->getId()." $j\n";
+
+            $this->getContainer()->get('etablissement.manager')->getOSMAdresse()->calculCoordonnees($etablissement->getAdresse());
+            if($i > 500) {
+                $this->dm->flush();
+                $i=0;
+            }
+            $i++;
+            $j++;
+        }
+
+        $this->dm->flush();
+    }
+}
--- project/src/AppBundle/Tool/OSMAdresses.php
+++ project/src/AppBundle/Tool/OSMAdresses.php
@@ -11,6 +11,9 @@ class OSMAdresses {
     private $lon;
     private $url;
     private $format;
+    
+    CONST RAYON_TERRE = 6378137;
+    CONST DISTANCE_CHECK = 20000;
 
     public function __construct($url, $format = 'json') {
         $this->url = $url;
@@ -30,24 +33,53 @@ class OSMAdresses {
             return false;
         }
         $fullAdresse = $adresseTrim.", ".$document->getCodePostal(). " ". $document->getCommune().', FRANCE';
+		$minAdresse = $document->getCodePostal(). " ". $document->getCommune().', FRANCE';
+       
 
-        $url = $this->url . '?q=' . urlencode($fullAdresse);
-
-        $file = file_get_contents($url);
-
-        $result = json_decode($file);
-        if (!count($result)) {
+        $result = $this->getCoordonnees($fullAdresse);
+        $resultMin = $this->getCoordonnees($minAdresse);
+        if (!$result) {
             return "Adresse non trouvée : $fullAdresse";
         }
-        if(!count($result->response->docs) || !isset($result->response->docs[0])){
-            return "Adresse non trouvée : $fullAdresse" ;
-        }
-
         $lat = $result->response->docs[0]->lat;
         $lon = $result->response->docs[0]->lng;
+        if ($resultMin) {
+        	$distance = round($this->getDistances($lat, $lon, $resultMin->response->docs[0]->lat, $resultMin->response->docs[0]->lng));
+        	if ($distance > self::DISTANCE_CHECK) {
+        		$lat = $resultMin->response->docs[0]->lat;
+        		$lon = $resultMin->response->docs[0]->lng;
+        	}
+        }
         $this->document->getCoordonnees()->setLat($lat);
         $this->document->getCoordonnees()->setLon($lon);
         return "Nouvelle coordonnées : $lat,$lon pour $fullAdresse";
+    }
+    
+    public function getDistances($lat1, $lon1, $lat2, $lon2) 
+    {
+		$rlo1 = deg2rad($lon1);
+	  	$rla1 = deg2rad($lat1);
+	  	$rlo2 = deg2rad($lon2);
+	  	$rla2 = deg2rad($lat2);
+	 	$dlo = ($rlo2 - $rlo1) / 2;
+	 	$dla = ($rla2 - $rla1) / 2;
+    	$a = (sin($dla) * sin($dla)) + cos($rla1) * cos($rla2) * (sin($dlo) * sin($dlo));
+    	$d = 2 * atan2(sqrt($a), sqrt(1 - $a));
+    	return (self::RAYON_TERRE * $d);
+    }
+    
+    public function getCoordonnees($adresse)
+    {
+    	$url = $this->url . '?q=' . urlencode($adresse);
+    	$file = file_get_contents($url);
+    	$result = json_decode($file);
+    	if (!count($result)) {
+    		return null;
+    	}
+    	if(!count($result->response->docs) || !isset($result->response->docs[0])){
+    		return null;
+    	}
+    	return $result;
     }
 
 
