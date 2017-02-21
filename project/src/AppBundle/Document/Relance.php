b33b53c2b0dfee4ab24cbc7a91e8cfc6fc8d4a24
Mathurin Petit <mathurin.petit@gmail.com>
2017-02-21 14:45:54 +0100

    ajout de l'embedDoc relance qui contient les dates des relances

---
+++ project/src/AppBundle/Document/Relance.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * @MongoDB\EmbeddedDocument
+ * @MongoDB\Index()
+*/
+class Relance {
+
+    /**
+     * @MongoDB\Date
+     */
+    protected $dateRelance;
+
+    /**
+     * @MongoDB\Int
+     */
+    protected $numeroRelance;
+
+
+    /**
+     * Set dateRelance
+     *
+     * @param date $dateRelance
+     * @return self
+     */
+    public function setDateRelance($dateRelance)
+    {
+        $this->dateRelance = $dateRelance;
+        return $this;
+    }
+
+    /**
+     * Get dateRelance
+     *
+     * @return date $dateRelance
+     */
+    public function getDateRelance()
+    {
+        return $this->dateRelance;
+    }
+
+    /**
+     * Set numeroRelance
+     *
+     * @param int $numeroRelance
+     * @return self
+     */
+    public function setNumeroRelance($numeroRelance)
+    {
+        $this->numeroRelance = $numeroRelance;
+        return $this;
+    }
+
+    /**
+     * Get numeroRelance
+     *
+     * @return int $numeroRelance
+     */
+    public function getNumeroRelance()
+    {
+        return $this->numeroRelance;
+    }
+}
