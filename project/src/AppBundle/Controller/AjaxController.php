bb135d63332125a4da1a2a85898b73d97e168d19
Gabriel Poma <gpoma@24eme.fr>
2019-02-05 18:35:51 +0100

    On passe directement les objets dans le template twig

--- project/src/AppBundle/Controller/AjaxController.php
+++ project/src/AppBundle/Controller/AjaxController.php
@@ -9,40 +9,23 @@ use AppBundle\Document\Passage;
 class AjaxController extends Controller
 {
     /**
-     * @Route("/ajax/passage/{secteur}/visualisation/{mois}", name="ajax_passage", defaults={"secteur"="PARIS"})
-     */
-
-    public function listPassageAction($secteur, $mois = null)
-    {
-    }
-
-    /**
      * @Route("/ajax/passage/{passage}/infos", name="ajax_more_infos_passage")
      */
     public function showInformationsAction(Passage $passage)
     {
-        $infossup = [];
-
         $etablissement = $passage->getEtablissement();
-        $infossup['etablissement']['nom'] = $etablissement->getNom();
-        $infossup['etablissement']['commentaire'] = $etablissement->getCommentaire();
-        $infossup['etablissement']['contact']['telephone'] = $etablissement->getTelephoneFixe();
-        $infossup['etablissement']['contact']['portable'] = $etablissement->getTelephonePortable();
-        $infossup['etablissement']['contact']['fax'] = $etablissement->getFax();
-        $infossup['etablissement']['contact']['libelle'] = $etablissement->getContactCoordonnee()->getLibelle();
-        $infossup['etablissement']['contact']['email'] = $etablissement->getEmail();
-
         $contrat = $passage->getContrat();
-        $infossup['contrat']['nomenclature'] = $contrat->getNomenclature();
-
         $societe = $contrat->getSociete();
-        $infossup['societe']['id'] = $societe->getIdentifiant();
-        $infossup['societe']['nom'] = $societe->getRaisonSociale();
-        $infossup['societe']['icon'] = $societe->getIcon();
-
         $facture = $this->get('facture.manager');
-        $infossup['facture']['retard'] = $facture->getRetardDePaiementBySociete($societe);
 
-        return $this->render('passage/infossupplementaires.html.twig', compact('infossup'));
+        return $this->render('passage/infossupplementaires.html.twig',
+            [
+                'passage' => $passage,
+                'etablissement' => $etablissement,
+                'contrat' => $contrat,
+                'societe' => $societe,
+                'retard' => count($facture->getRetardDePaiementBySociete($societe)) > 0
+            ]
+        );
     }
 }
