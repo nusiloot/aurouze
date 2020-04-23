958baf7547f63f4436341483786c3b2ff362a214
Gabriel Poma <gpoma@24eme.fr>
2020-04-23 18:21:24 +0200

    Requirement Interface Facturable

--- project/src/AppBundle/Model/FacturableInterface.php
+++ project/src/AppBundle/Model/FacturableInterface.php
@@ -2,8 +2,33 @@
 
 namespace AppBundle\Model;
 
+use AppBundle\Document\Societe;
+use AppBundle\Document\Compte;
+use AppBundle\Document\Soussigne;
+
 interface FacturableInterface
 {
+    public function getSociete();
+    public function setSociete(Societe $societe);
+
+    public function getCommercial();
+    public function setCommercial(Compte $commercial);
+
+    public function getEmetteur();
+    public function setEmetteur(Soussigne $emetteur);
+
+    public function getDestinataire();
+    public function setDestinataire(Soussigne $destinataire);
+
+    public function getDateEmission();
+    public function setDateEmission($date);
+
+    public function getMontantHT();
+    public function setMontantHT($montantHT);
+
+    public function getMontantTTC();
+    public function setMontantTTC($montantTTC);
+
     public function getDocumentType();
     public function getLignes();
     public function getNumero();
